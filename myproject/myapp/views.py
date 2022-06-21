7e3a792781f58f30af945993d97e9530564eedbe
Ráez <raez.dev@gmail.com>
2022-06-21 20:55:08 +0200

    Apartados 3 y 4

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,5 +3,12 @@ from django.http import HttpResponse, response
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hola, Bienvenido</h1>')
+    # return HttpResponse('<h1>Hola, Bienvenido</h1>')
+
+    context = {
+         'name': 'Ráez',
+         'age': 37,
+         'pais': 'Spain'
+    }
+    return render(request, 'index.html', context)
 
